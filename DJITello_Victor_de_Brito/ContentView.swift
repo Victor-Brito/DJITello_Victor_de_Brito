82367c4f13038c808b1fdb950c9c9f3457e036b2
Victor de Brito Marinho Correa <victordevelopersoftware@gmail.com>
2022-05-07 13:31:23 -0300

    Initial Commit

---
+++ DJITello_Victor_de_Brito/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DJITello_Victor_de_Brito
+//
+//  Created by Victor Brito on 07/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DJITello_Victor_de_Brito/DJITello_Victor_de_BritoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJITello_Victor_de_BritoApp.swift
+//  DJITello_Victor_de_Brito
+//
+//  Created by Victor Brito on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct DJITello_Victor_de_BritoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
