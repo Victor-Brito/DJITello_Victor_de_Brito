3cef342cf468d9c1e9fd7c499c23b87b07932690
Victor de Brito Marinho Correa <victordevelopersoftware@gmail.com>
2022-05-07 15:18:16 -0300

    created: UDP connection with tello and injection dependency to initializer DJI Tello propriety

--- DJITello_Victor_de_Brito/View/ContentView.swift
+++ DJITello_Victor_de_Brito/View/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var commands: TelloComands
+    
     var body: some View {
-        Text("Teste")
+        HStack {
+            commandView
+        }
+    }
+    
+    var commandView: some View {
+        VStack {
+            Button("Connect") {
+                commands.connectPressed()
+            }
             .padding()
+            Button("Command") {
+                commands.commandPressed()
+            }
+            .padding()
+            Button("Takeoff") {
+                commands.takeoffPressed()
+            }
+            .padding()
+            Button("Land") {
+                commands.landPressed()
+                
+            }
+            .padding()
+            Button("Rotate 360° clockwise") {
+                commands.rotate360CwPressed()
+            }
+            .padding()
+            Button("Rotate 360° counterclockwise") {
+                commands.rotate360CcwPressed()
+            }
+
+//            Form {
+//                Section {
+//                    Button("Takeoff") {
+//                        commands.takeoffPressed()
+//                    }
+//                    Button("Land") {
+//                        commands.landPressed()
+//
+//                    }
+//                }
+//                Section {
+//                    Button("Rotate 360° clockwise") {
+//                        commands.rotate360CwPressed()
+//                    }
+//                    Button("Rotate 360° counterclockwise") {
+//                        commands.rotate360CcwPressed()
+//                    }
+//
+//                }
+//            }
+        }
     }
 }
 
---
+++ DJITello_Victor_de_Brito/ViewModel/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DJITello_Victor_de_Brito
+//
+//  Created by Victor Brito on 07/05/22.
+//
+
+import Combine
+import SwiftUI
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var interactionReceiver = TelloComands()
+    var commandSender = UdpSender(data: TelloAddress(host: "192.168.10.1", port: 8889))
+    
+    var cancellables = [AnyCancellable]()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        interactionReceiver.$connect.sink(receiveValue: self.commandSender.connect).store(in: &cancellables)
+        interactionReceiver.$command.sink(receiveValue: self.commandSender.send).store(in: &cancellables)
+        
+        Timer.publish(every: 5, on: .main, in: .default)
+            .autoconnect()
+            .sink(receiveValue: { timer in
+                self.commandSender.send("command".data(using: .utf8)!)
+            }).store(in: &cancellables)
+        
+        return true
+    }
+}
